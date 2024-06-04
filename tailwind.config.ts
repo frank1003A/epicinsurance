6dd1d8f3dbeef8b19521d19fb1a9eaff2e5320c3
Frank Ezene <ezenefrankc@gmail.com>
2024-06-04 14:02:39 +0100

    update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,25 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      padding: {
+        epic_page_desk: "5rem",
+        epic_page_mob: "1.5rem",
+      },
+      colors: {
+        epic_primary: "#582849",
+        epic_secondary: "#ff4313",
+        epic_variant: "#ffdeb0",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "custom-gradient": "linear-gradient(to right, white 75%, #582849 30%)",
+        "custom-gradient-to-b":
+          "linear-gradient(to right, white 60%, #582849 40%)",
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
 export default config;
